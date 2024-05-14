d0913a166f2b8c4442ee3ca60f2ff46c464bd2c5
divyabharathi.mathav <divyabharathi.mathav@DIVYA-FCLZKR3>
2024-05-14 16:37:30 +0530

    First commit

---
+++ DBOperation/src/dboperationpack/DBConnectionexample.java
@@ -0,0 +1,27 @@
+package dboperationpack;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConnectionexample {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		// TODO Auto-generated method stub
+        //connect with DB
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection connect = DriverManager.getConnection("jdbc:mysql://root@localhost/selenium_user");
+		//jdbc:mysql://localhost:3306/selenium_user","root"."root"
+	     Statement sta = connect.createStatement();
+	     ResultSet   result   = sta.executeQuery("select * From My_Tablename");
+	     
+	     while(result.next()){
+	    	 System.out.println("Name:"+result.getString(1));
+	    	 
+	     }
+	 
+	}
+
+}
